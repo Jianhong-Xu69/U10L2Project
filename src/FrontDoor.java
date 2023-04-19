a9ed71f85a391417bcd60d51408873d11247170b
BT_4N2_15 <jxu5406@bths.edu>
2023-04-19 12:27:31 -0400

    Finished last exploration question

--- src/FrontDoor.java
+++ src/FrontDoor.java
@@ -2,8 +2,8 @@ import java.util.Locale;
 
 public class FrontDoor {
     public static void main(String[] args) {
-        int[] nums = {2,5,6};
-        System.out.println(countEvens(nums, 2));
+        int[] nums = {10000000,5,6,7,8,1,1,2,5,4,3,2};
+        System.out.println(findMaximum(nums, 9));
     }
 
     public static boolean isPalindrome(String check) {
@@ -28,6 +28,18 @@ public class FrontDoor {
             return 1 + countEvens(array, i - 1);
         } else {
             return countEvens(array, i - 1);
+        }
+    }
+
+    public static int findMaximum(int[] array, int i) {
+        if (i == array.length-1){
+            return array[i];
+        }
+        int temp = findMaximum(array, i+1);
+        if (temp >= array[i]){
+            return temp;
+        } else {
+            return array[i];
         }
     }
 }
